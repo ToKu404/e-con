eb98e460ed958130640d08470dbeef14421af60c
toku404 <phantom26isn@gmail.com>
2022-12-27 23:59:45 +0800

    fix: bug

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,4 +1,4 @@
 class ApiService {
-  static String baseUrlFinalExam = 'http://34.101.133.233:8080/api/v0';
+  static String baseUrlFinalExam = 'http://34.101.133.233:8080/api/v0 ';
   static String baseUrlCPL = 'https://api.cpl.npedigihouse.tech/api';
 }
